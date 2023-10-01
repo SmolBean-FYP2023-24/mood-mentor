09f89af70bb75a6a29121ccc1aecfe88ce65b5da
Sarthak Singhal <sarthaksinghal712@gmail.com>
2023-10-01 22:40:16 +0800

    Create ML_1st_try.py
    Added from Khushi's Repository

---
+++ ML_1st_try.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+import os
+import sys
+
+# librosa is a Python library for analyzing audio and music. It can be used to extract the data from the audio files we will see it later.
+import librosa
+import librosa.display
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+
+# to play the audio files
+from IPython.display import Audio
+
+import keras
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten, Dropout, BatchNormalization
+#from tensorflow.keras.utils  import np_utils #np_utils module has been depreceated 
+from tensorflow.keras.utils  import  to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+import warnings
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
