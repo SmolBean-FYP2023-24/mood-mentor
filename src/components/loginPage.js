84e7b6fd00cf60bd2db74ff9b2482227beef135c
sarthaksinghal712 <sarthaksinghal712@gmail.com>
2023-12-20 18:38:39 +0530

    update

--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -18,12 +18,12 @@ export default function Login(props) {
         <div className="p-0 col-md-6 bg-light" id="left_side">
           {/* <div>Hello</div> */}
         </div>
-        {/* <div className="col-sm-12 col-md-6 p-0 bg-light">
-          <LoginForm handleUser={handleUser} />
-        </div> */}
         <div className="col-sm-12 col-md-6 p-0 bg-light">
-          <SignUp handleUser={handleUser} />
+          <LoginForm handleUser={handleUser} />
         </div>
+        {/* <div className="col-sm-12 col-md-6 p-0 bg-light">
+          <SignUp handleUser={handleUser} />
+        </div> */}
       </div>
     </div>
   );
