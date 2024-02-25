ded233ecced21ac93b65a99c1b7617a2f900928f
simar-g <sgangar@connect.ust.hk>
2024-02-25 14:52:33 +0800

    Simar_v1: dashboard base setup

---
+++ src/components/dashboard.js
@@ -0,0 +1,18 @@
+import {React, useEffect, useState} from "react";
+import './dashboard.css';
+
+const emotions={
+    1:'neutral', 
+    2:'happy', 
+    3:'sad', 
+    4:'angry',
+    5:'fear',
+    6:'disgust'
+  };
+
+  function dashboard()
+  {
+    
+  }
+
+export default dashboard;
