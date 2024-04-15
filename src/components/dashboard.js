09e86486b0d6fd8cdbb0779feadb9d0a990c290b
simar-g <sgangar@connect.ust.hk>
2024-04-15 14:44:31 +0800

    New Dashbaord in process

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef , useState} from "react";  
+import { React, useEffect, useState } from "react"; 
 import { Line } from 'react-chartjs-2';
 import { Bar } from 'react-chartjs-2';
 import "@aws-amplify/ui-react/styles.css";
@@ -11,344 +11,329 @@ import './styles/dashboard.css';
 import "./styles/profilePage.css";
 import MenuChart from './MenuChart.js';
 import ProfilePictureSection from "./ProfilePictureSection.js";
+import { dummyData } from './dummyData'; // Import the dummy data
+import img from './images/colored/badge200qs.png';
+import img1 from './images/greyed-out/badge200qs.png';
 
 
 import { evaluate, parse, sqrt, exp, pi } from 'mathjs';
+import { components } from "@aws-amplify/ui-react";
 
 
 
 
 
+function BadgeHolder({ badges }) {
+  const [currentBadgeIndex, setCurrentBadgeIndex] = useState(0);
 
-
-function generateNormalDistributionData(mean, standardDeviation, minX, maxX, step) {
-  const data = [];
-  for (let x = minX; x <= maxX; x += step) {
-    const exponent = -((x - mean) ** 2) / (2 * standardDeviation ** 2);
-    const y = (1 / (standardDeviation * sqrt(2 * pi))) * exp(exponent);
-    data.push(y);
-  }
-  return data;
-}
-
-
-function NormalDistributionChart({ mean, standardDeviation, minX, maxX, step }) {
-  const data = generateNormalDistributionData(mean, standardDeviation, minX, maxX, step);
-
-  const chartData = {
-    labels: data.map(point => point.x),
-    datasets: [
-      {
-        label: 'Normal Distribution',
-        data: data.map(point => point.y),
-        fill: false,
-        borderColor: 'rgba(75, 192, 192, 1)',
-        tension: 0.1,
-      },
-    ],
+  const navigateBadge = (direction) => {
+    if (direction === 'prev') {
+      setCurrentBadgeIndex((prevIndex) => prevIndex - 1);
+    } else if (direction === 'next') {
+      setCurrentBadgeIndex((prevIndex) => prevIndex + 1);
+    }
   };
-
-  const chartOptions = {
-    responsive: true,
-    scales: {
-      x: {
-        title: {
-          display: true,
-          text: 'X',
-        },
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'Y',
-        },
-      },
-    },
-  };
-
-  return <Line data={chartData} options={chartOptions} />;
-}
-
-
-function MyChart() {
-  const chartRef = useRef(null);
 
   useEffect(() => {
-    const canvas = chartRef.current;
-    const ctx = canvas.getContext('2d');
-    let myBarChart = null;
-
-    // Base colors
-    const baseColors = ["#50C4ED", "#387ADF", "#333A73"];
-
-    // Generate shades of colors based on the base colors
-    const colorScale = chroma.scale(baseColors).mode('lch').colors(10);
-    function createGlossyColor(color) {
-      const glossyColor = chroma(color).alpha(0.6).css();
-      return glossyColor;
-    }
-
-    const userIndex = 4; // Index of the user's percentile (e.g., 4 for 60%)
-    const backgroundColor = colorScale.map((color, index) =>
-    index === userIndex ? createGlossyColor(color) : baseColors[1]
-    );
-
-    
-
-
-
-    const data = {
-      labels: [
-        "20%",
-        "30%",
-        "40%",
-        "50%",
-        "60%",
-        "70%",
-        "80%",
-        "90%",
-        "100%"
-      ],
-      datasets: [
-        {
-          data: [
-            4,
-            8,
-            15,
-            30,
-            40,
-            30,
-            15,
-            8,
-            4
-          ],
-          backgroundColor,
-        }
-      ]
-    };
-
-    const options = {
-      tooltips: {
-        enabled: false
-      },
-      legend: {
-        display: false
-      },
-      annotation: {
-        annotations: [
-          {
-            type: "line",
-            mode: "vertical",
-            scaleID: "x-axis-0",
-            value: "70%",
-            borderColor: "black",
-            label: {
-              content: "Your Score",
-              enabled: true,
-              position: "center"
-            }
-          }
-        ]
-      },
-      scales: {
-        yAxes: [
-          {
-            display: false
-          }
-        ],
-        xAxes: [
-          {
-            barPercentage: 1.0,
-            categoryPercentage: 1.0,
-            gridLines: {
-              display: false
-            },
-            scaleLabel: {
-              display: true,
-              labelString: "Average Score"
-            }
-          }
-        ]
+    const handleKeyDown = (event) => {
+      if (event.key === 'ArrowLeft') {
+        navigateBadge('prev');
+      } else if (event.key === 'ArrowRight') {
+        navigateBadge('next');
       }
     };
 
-    if (canvas && canvas.id === 'myChart') {
-      // Destroy existing chart instance if it exists
-      if (Chart.instances[0]) {
-        Chart.instances[0].destroy();
-      }
-
-      // Create new chart instance
-      myBarChart = new Chart(ctx, {
-        type: 'bar',
-        data,
-        options,
-      });
-    }
+    document.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      // Cleanup code to destroy the chart instance
-      if (myBarChart) {
-        myBarChart.destroy();
-      }
+      document.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
 
-  return <canvas ref={chartRef} id="myChart" />;
-}
+  const currentBadge = Object.entries(badges)[currentBadgeIndex];
+  const [badgeName, badgeValue] = currentBadge;
 
+  console.log(badgeName)
+  console.log(badgeValue)
+
+  // C:\Users\Lenovo S540 FSIN\Desktop\1_FYP_2023\FYP_Final\mood-mentor\src\components\images\greyed-out\badge200qs.png
+  
+
+  return (
+      <div className="badge">
+        {badgeValue ? (
+          <img src={img} alt={badgeName} style={{ width: '20px', height: '20px' }} />
+        ) : (
+          <img src={img1} alt={badgeName} style={{ width: '20px', height: '20px' }} />
+        )}
+        <span className="arrow left-arrow" onClick={() => navigateBadge('prev')}>&larr;</span>
+        <span className="arrow right-arrow" onClick={() => navigateBadge('next')}>&rarr;</span>
+      </div>
+  );
+
+  // C:\Users\Lenovo S540 FSIN\Desktop\1_FYP_2023\FYP_Final\mood-mentor\src\components\images\greyed-out\badge50qs.png
+
+}
 
 // Dashboard function starts here
 
 function Dashboard({ user }) {
 
-  
-  // CHART2: LINE CHART FOR QUESTIONS PER WEEK
+  // Access the variables from the dummy data
+  const {
+    id,
+    username,
+    password,
+    streak,
+    level,
+    badges,
+    speakingQuestion,
+    listeningQuestion,
+    conversationQuestion,
+    hasOnboarded,
+    speakingAccuracy,
+    listeningAccuracy,
+    conversationAccuracy,
+  } = dummyData;
 
-  const data_qs_per_week = {
-    labels: [1, 2, 3, 4,5,6,7], // Week numbers
-    legend: {
-      display: false, // Hide the legend
-    },
-    datasets: [
-      {
-        label:'Number of Questions Practiced',
-        data: [10, 15, 8, 12, 20, 16, 25], // Number of questions practiced per week
-        fill: true,
-        // const baseColors = ["#50C4ED", "#387ADF", "#333A73"];
-        backgroundColor: 'rgba(56, 122, 223, 0.7)',
-        borderColor: 'rgba(56, 122, 223, 1)',
-        borderWidth: 2,
-        lineTension: 0.3, // Adjust the line tension to control the curve smoothness
-      },
-    ],
+  // Function to create  menus
+  const [selectedExercise, setSelectedExercise] = useState('Speaking');
+
+  const handleExerciseChange = (event) => {
+    setSelectedExercise(event.target.value);
   };
 
-  const options = {
-    responsive: true,
-    scales: {
-      x: {
-        title: {
-          display: true,
-          text: 'Week Number',
-        },
+
+
+  // Below is the code for the accuracy charts
+
+  const [selectedExercise_acc, setSelectedExercise_acc] = useState('Speaking');
+  const [accuracies, setAccuracies] = useState([]);
+  const [trendIndicators, setTrendIndicators] = useState([]);
+
+  const emotion_labels=['happy', 'sad', 'angry', 'fear', 'disgust', 'surprise']
+
+  useEffect(() => {
+    updateAccuracies(selectedExercise_acc);
+  }, [selectedExercise_acc]);
+
+  const handleExerciseChange_acc = (event) => {
+    setSelectedExercise_acc(event.target.value);
+  };
+
+  const calculateEmotionAccuracies = (exercise) => {
+    const accuracyData = dummyData[`${exercise.toLowerCase()}Accuracy`];
+
+    const labels = Object.keys(accuracyData);
+    // console.log(labels)
+    const accuracyValues = Object.values(accuracyData);
+
+    // Calculate the trend indicators
+    const trendIndicators = calculateTrendIndicators(accuracyValues);
+
+    return {
+      labels,
+      accuracies: accuracyValues,
+      trendIndicators,
+    };
+  };
+
+  const calculateTrendIndicators = (accuracyValues) => {
+    const trendIndicators = [];
+    for (let i = 0; i < accuracyValues.length; i++) {
+      if (i === 0) {
+        trendIndicators.push(null);
+      } else {
+        if (accuracyValues[i] > accuracyValues[i - 1]) {
+          trendIndicators.push('upward');
+        } else if (accuracyValues[i] < accuracyValues[i - 1]) {
+          trendIndicators.push('downward');
+        } else {
+          trendIndicators.push('equal');
+        }
+      }
+    }
+    return trendIndicators;
+  };
+
+  const updateAccuracies = (exercise) => {
+  const { labels, accuracies, trendIndicators } = calculateEmotionAccuracies(exercise);
+
+    setAccuracies(accuracies);
+    setTrendIndicators(trendIndicators);
+
+    // Destroy the existing chart if it exists
+    const existingChart = Chart.getChart("accuracy-chart");
+    if (existingChart) {
+      existingChart.destroy();
+    }
+
+    // Update the chart
+    const accuracyChart = document.getElementById('accuracy-chart').getContext('2d');
+    new Chart(accuracyChart, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: `${exercise} Accuracy`,
+            data: accuracies,
+            backgroundColor: 'rgba(54, 162, 235, 0.5)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+            pointRadius: 3,
+            pointHoverRadius: 5,
+          },
+        ],
       },
-      y: {
-        title: {
-          display: true,
-          text: 'Number of Questions Practiced',
-        },
-      },
-    },
-    plugins: {
-      tooltip: {
-        callbacks: {
-          label: (context) => {
-            const label = context.dataset.label || '';
-            if (label) {
-              return `${label}: ${context.parsed.y}`;
-            }
-            return `${context.parsed.y}`;
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+              callback: function (value) {
+                return (value * 100).toFixed(0) + '%';
+              },
+            },
           },
         },
       },
-    },
+    });
   };
-
-
-
-  
 
 
   return (
     <div className="container-fluid-dashboard">
-      {/* <h1 className="top-text">Dashboard</h1> */}
-  <div className="row-dashboard">
-      <div className="profile-box-dashboard">
-        <div className="profile-header">
+       <div className="row-1-dashbaord">
+        <div className="col-lg-2 col-md-4">
+          <div className="sidebar-dashboard">
+            <div> clas
 
-          <div className="profile-info">
-               <ProfilePictureSection/>
-            <h2 className="welcome-text"> 
-                WELCOME BACK !
-                <div class="dashboard-user-data"> 
-                <h2> Jojo Gomez</h2>
-                <h2>Streak: Week1 </h2>
-                </div> {/* extract the value from the DB here */}
-                {/* extract the value from the DB here */}
-            </h2>
-          </div>
-          
-         
-        </div>
-        
-      </div>
-
-      <div className="normal-box-dashboard">
-            {/* <p>We put the badges here </p> */}
-        <div className='badge-text-row'>
-               <h2 className="welcome-text"> ACHIEVEMENTS </h2>
-        </div>
-
-        <div className="badges">
-
-        <div className="badge1">
-            <img src="https://imgur.com/sPI6W5u.png" alt="Embedded Image"/>
-          </div>
-
-          <div className="badge2">
-            <img src="https://imgur.com/sPI6W5u.png" alt="Embedded Image"/>
+            </div>
+            <h2>Navigation</h2>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Services</li>
+              <li>Contact</li>
+            </ul>
           </div>
         </div>
-        </div> 
-  </div>
-  {/* end of the row tag */}
+        <div className="col-lg-12 col-md-12 col-sm-12">
+          {/* Main content goes here */}
+          <div className="main-content">
+            <div className="row-2-dashboard">
+              <div className="badge-holder-dashboard">
+                 <BadgeHolder badges={badges}/>
+             
 
-
-  <div className="custom-row">
-
-      <a href="/lex">
-        <button className="profile-button">Listening Exercise</button>
-      </a>
-      <a href="/eex">
-        <button className="profile-button">Speaking Exercise</button>
-      </a>
-      <a href="/cex">
-        <button className="profile-button">Conversational Exercise</button>
-      </a>
-  </div>
-
-  <div className="custom-row-1">
-
-      <div className="chart-box-normal">
-              <div className="chart-container">
-                  {/* <Bar data={chartData} options={chartOptions} /> */}
-                  <MyChart />
               </div>
-      </div> 
+              <div className="qs-emotion-dashboard">
 
-      <div className="chart-box-practice-qs">
-            <Line data={data_qs_per_week} options={options} />
-      </div>    
+              <div className="qs-emotion-dashboard-row1">
 
-  </div>
+              <div className="qs-emotion-dashboard-heading">Number of Questions by Emotion</div>
+              <div className="exercise-dropdown">
+              {/* <label htmlFor="exercise-select">Select Exercise:</label> */}
+              <select
+                id="exercise-select"
+                value={selectedExercise}
+                onChange={handleExerciseChange}
+              >
+                <option value="Speaking">Speaking</option>
+                <option value="Listening">Listening</option>
+                <option value="Conversation">Conversation</option>
+              </select>
+            </div>
 
-  <div className="custom-row-2"> 
-   
-      <MenuChart />
-    
-  </div>
+        </div>
+              
+
+             <div className="qs-emotion-dashbaord-inner">
+
+             <Bar
+            data={{
+              labels: ['Happy', 'Sad', 'Angry', 'Disgust', 'Surprise', 'Fear'],
+              datasets: [
+                {
+                  label:`${selectedExercise} Questions`,
+                  data: Object.values(
+                    dummyData[selectedExercise.toLowerCase() + 'Question']
+                  ),
+                  backgroundColor: 'rgba(54, 162, 235, 0.5)', // Color for the selected exercise Questions bars
+                },
+              ],
+            }}
+            options={{
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  ticks: {
+                    precision: 0, // Display integers for y-axis ticks
+                  },
+                },
+              },
+              plugins: {
+                legend: {
+                  display: false, // Disable the legend
+                },
+              },
+            }}
+          />
 
 
- 
+
+      </div>
+           
+
+              </div>
+
+            </div>
+            {/* end of row2 */}
+
+            <div className="row-3-dashboard">
+              <div className="acc-graph-dashboard">
+
+               <canvas id="accuracy-chart"></canvas>
+             
+
+              </div>
+              <div className="acc-stats-dashboard">
+          <h2>Accuracy Statistics</h2>
+          <div className="dropdown-container">
+            <label htmlFor="exercise-select">Select Exercise:</label>
+            <select
+              id="exercise-select"
+              value={selectedExercise_acc}
+              onChange={handleExerciseChange_acc}
+            >
+              <option value="Listening">Listening</option>
+              <option value="Speaking">Speaking</option>
+              <option value="Conversation">Conversation</option>
+            </select>
+          </div>
+          <div className="emotion-accuracies">
+            <h3>Emotion Accuracies</h3>
+            {accuracies.map((accuracy, index) => (
+              <div className="emotion-row" key={index}>
+                <div className="emotion-label">{emotion_labels[index]}:</div>
+                <div className={`emotion-value ${trendIndicators[index]}`}>{(accuracy * 100).toFixed(2)}%</div>
+              </div>
+            ))}
+          </div>
+        {/* </div>
+      </div> */}
 
 
-  
 
-      
-       
-      
-      
+              </div>
+
+            </div>
+
+          </div>
+          {/* end of main content */}
+        </div>
+      </div>
     </div>
     // end of the container-fluid tag
 
